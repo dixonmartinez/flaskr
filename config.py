ee6ff4779ffcada12d6d200dbd8c09a500750074
Dixon Martinez <dixon22martinez@gmail.com>
2024-03-13 15:14:01 -0400

    Update ckeditor from local server

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # export SECRET_KEY="your secret key"
@@ -12,7 +11,11 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CKEDITOR_PKG_TYPE = 'standard'  # basic, standard, full, standard-all, full-allç
+    CKEDITOR_PKG_TYPE = 'standard'  # basic, standard, full, standard-all, full-all
+    # Flag used to enable serving resources from local when use ckeditor.load(), default is to retrieve from CDN.
+    CKEDITOR_SERVE_LOCAL = True
+    # CKEDITOR_CUSTOM_URL = 'static/ckeditor/ckeditor.js'
+    CKEDITOR_CUSTOM_URL = '3static/ckeditor/full/ckeditor.js'
     UPLOAD_FOLDER = 'app/static/images/'
     MAX_CONTENT_LENGTH = 1024 * 1024
     UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif']
